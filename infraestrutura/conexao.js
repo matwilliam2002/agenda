9114dd57d444d4a6a93fc042cf911308f4029e71
Gabriel <106440419+Gadu05@users.noreply.github.com>
2024-10-19 19:24:30 +0000

    folders update

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -8,5 +8,13 @@ const conexao = mysql.createConnection({
     database:"agenda"
 })
 
+conexao.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+});
+
 // export default conexao;
 module.exports = conexao;
